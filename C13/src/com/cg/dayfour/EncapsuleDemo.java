15eb6cd286c1012d21310a5811311fd68bda62bc
ELCOT <ELCOT@Lenovo>
2023-07-13 10:57:54 +0530

    day3

---
+++ C13/src/com/cg/dayfour/EncapsuleDemo.java
@@ -0,0 +1,18 @@
+package com.cg.dayfour;
+
+public class EncapsuleDemo {
+
+	public static void main(String[] args) {
+		
+		Encapsule cap = new Encapsule();
+		
+		cap.setName("Kishore");
+		cap.setAge(20);
+		cap.setGender("Male");
+		cap.setNo(1);
+		
+		System.out.println(cap);
+		
+	}
+
+}
---
+++ C13/src/com/cg/daytwo/taxclass.java
@@ -0,0 +1,10 @@
+package com.cg.daytwo;
+
+public class taxclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
